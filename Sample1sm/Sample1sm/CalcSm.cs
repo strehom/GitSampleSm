7c329e349b2e1b8a6d875e8863237013063153b5
mate.streho <mate.streho@semilab.hu>
2018-05-16 09:26:06 +0200

    feat: create div method

--- Sample1sm/Sample1sm/CalcSm.cs
+++ Sample1sm/Sample1sm/CalcSm.cs
@@ -12,5 +12,12 @@ namespace Sample1sm
         {
             Console.WriteLine("This is the Add method.");
         }
+
+        public void div()
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine("This is the Div method: " + a+b);
+        }
     }
 }
