4224d04d8eb407ad891122532df8fb85ba00f339
mate.streho <mate.streho@semilab.hu>
2018-05-16 11:02:16 +0200

    feat: create divD method

--- Sample1sm/Sample1sm/CalcSm.cs
+++ Sample1sm/Sample1sm/CalcSm.cs
@@ -17,15 +17,20 @@ namespace Sample1sm
         {
             int a = 2;
             int b = 3;
-            Console.WriteLine("This is the Div method: " + a+b);
+            Console.WriteLine("This is the Div method: " + a + b);
         }
 
         public void divB()
         {
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(i * 2);
+                Console.WriteLine("This is the DivB method: " + i * 2);
             }
+        }
+
+        public void divD()
+        {
+            Console.WriteLine("This is the DivD method.");
         }
     }
 }
